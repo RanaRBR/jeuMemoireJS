08c564593763631ae4e2c7689542104eaaf1780f
RanaRBR <boutrosrafaelrana@gmail.com>
2025-02-05 11:02:35 +0100

    suit

--- public/js/main.js
+++ public/js/main.js
@@ -1,51 +1,21 @@
-let startGameContenair = document.querySelector(".startGame");
-let startGameCards = document.querySelectorAll(".startGame .card");
-let startGame = document.querySelector(".startGame button");
-let playground = document.querySelector(".playGround");
 
-let levels=2, 
-    column=3, 
-    row=4, 
-    matched=0, 
-    cardOne, 
-    cardTwo,
-    IsPreventClick = true;
+let levelCards = document.querySelectorAll(".startGame .card");
+let btnReplay = document.querySelector("#btnReplay");
+let home = document.getElementById("selectLevel");
+let field = document.getElementById("field");
 
-    startGameCards.forEach(element =>{
-        element.addEventListener("click", (e) =>{
-            startGameCards.forEach(el =>{
-                el.classList.remove
-            })
+levelCards.forEach(card => {
+    card.addEventListener("click", function() {
+        home.classList.add("hidden"); 
+        field.classList.remove("hidden");
+    });
+});
 
-            e.target.parentElement.classList.add("active");
-            levels = e.target.parentElement.getAttribute("level");
-            column = e.target.parentElement.getAttribute("column");
-            row = e.target.parentElement.getAttribute("row");
-        })
-    })
+btnReplay.addEventListener("click", function() {
+    home.classList.remove("hidden"); 
+    field.classList.add("hidden");
+});
 
 
-    startGame.addEventListener("click", (e) =>{
-        startGameContenair.style.display = "none";
-        playground.style.display ="grid";
 
-        createCards();
 
-    })
-
-    function createCards(){
-        let cardArr =[
-            "maison",
-            "bombe",
-            "cadeau",
-            "personne",
-            "avion",
-        ]
-        shuffleCards([...cardArr,...cardArr])
-    }
-
-    function shuffleCards(cards){
-
-    }
-
-    
